c4a22599a4e300c3d557920efc81045290540a01
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-08-05 02:38:23 -0300

    feat: add slide action tp alert dialog

--- front-end/src/components/Alert/index.tsx
+++ front-end/src/components/Alert/index.tsx
@@ -1,11 +1,21 @@
-import { Alert as MUIAlert, Snackbar } from '@mui/material';
+import { Alert as MUIAlert, Slide, Snackbar } from '@mui/material';
+import { TransitionProps } from '@mui/material/transitions';
 import useAlert from '../../hooks/useAlert';
+
+function SlideTransition(props: TransitionProps) {
+  return <Slide {...props} direction="up" />;
+}
 
 export default function Alert() {
   const { message, handleClose } = useAlert();
 
   return (
-    <Snackbar open={!!message} autoHideDuration={6000} onClose={handleClose}>
+    <Snackbar
+      open={!!message}
+      autoHideDuration={6000}
+      TransitionComponent={SlideTransition}
+      onClose={handleClose}
+    >
       <MUIAlert
         onClose={handleClose}
         severity={message?.type || 'error'}
