bbce2330bbb6faf4be026fb68a5d161965149ee2
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-08-05 01:24:49 -0300

    feat: signup page

--- front-end/src/components/pages/SignUp/index.tsx
+++ front-end/src/components/pages/SignUp/index.tsx
@@ -1,32 +1,83 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AxiosError } from 'axios';
+import api from '../../../services/api';
 import * as S from '../Login/style';
+import useAlert from '../../../hooks/useAlert';
 
 export default function SignUp() {
   const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const { setMessage } = useAlert();
+
+  async function handleSubmit(e : React.FormEvent) {
+    e.preventDefault();
+    setMessage(null);
+
+    if (!email || !password || !confirmPassword || !name) {
+      setMessage({ type: 'error', text: 'Todos os campos são obrigatórios!' });
+      console.log(1);
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setMessage({ type: 'error', text: 'As senhas devem ser iguais!' });
+      console.log(2);
+      return;
+    }
+
+    try {
+      await api.post('/signup', {
+        name,
+        email,
+        password,
+      });
+      setMessage({ type: 'success', text: 'Usuário criado com sucesso!' });
+      navigate('/');
+    } catch (error: Error | AxiosError | any) {
+      if (error.response) {
+        setMessage({
+          type: 'error',
+          text: error.response.data,
+        });
+      }
+    }
+  }
 
   return (
     <S.LoginWrapper>
       <S.Title>Easy My Party</S.Title>
-      <S.Form>
+      <S.Form onSubmit={handleSubmit}>
         <S.Input
           type="text"
           placeholder="nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <S.Input
           type="email"
           placeholder="e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <S.Input
           type="password"
           placeholder="senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <S.Input
           type="password"
           placeholder="confirmar senha"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />
-        <S.Button>Cadastrar</S.Button>
+        <S.Button type="submit">Cadastrar</S.Button>
         <h2 className="link" onClick={() => navigate('/')}>
           Já tem uma conta? Entre agora!
         </h2>
