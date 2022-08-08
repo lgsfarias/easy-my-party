8cbbb2ad5db603c82942a0de22f8726f87857996
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-08-08 16:41:02 -0300

    feat: config sendGrid to send invitation email with dynamic template

--- back-end/src/routers/index.ts
+++ back-end/src/routers/index.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import sgMail from '@sendgrid/mail';
 import authRouter from './AuthRouter';
 import partyRouter from './PartyRouter';
 import taskRouter from './TaskRouter';
@@ -8,6 +9,39 @@ const router = Router();
 router.get('/', (req: Request, res: Response) => {
   res.send('Easy my Party API');
 });
+router.post('/sendGridTest', async (req: Request, res: Response) => {
+  const apiKey = String(process.env.SENDGRID_API_KEY);
+  sgMail.setApiKey(apiKey);
+  const msg = {
+    to: 'lgsfarias.dev@gmail.com',
+    from: 'lgsfarias.dev@gmail.com',
+    templateId: 'd-b50d3d36c409443ebf4cf66683d28cb6',
+    dynamic_template_data: {
+      party: {
+        name: '[Nome da Festa]',
+        datetime: {
+          date: '[data da festa]',
+          time: '[hora da festa]',
+        },
+        adress: {
+          street: '[Rua, numero e complement]',
+          city: '[Cidade]',
+          state: '[Estado]',
+        },
+      },
+      confirmationlink: 'https://google.com',
+    },
+  };
+
+  try {
+    await sgMail.send(msg);
+    res.send('Email enviado com sucesso');
+  } catch (error: any) {
+    console.log(error.response.body.errors);
+    res.send('Erro ao enviar email');
+  }
+});
+
 router.use(authRouter);
 router.use(partyRouter);
 router.use(taskRouter);
